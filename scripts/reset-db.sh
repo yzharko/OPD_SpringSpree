bc3c8aac5c7503ae5dbf2d2dea1de278c19a7209
AlexNik <sahan@DESKTOP-R8OTJOJ>
2025-03-11 16:09:40 +0300

    OPD_SS: изменение иерархии файлов связанных с docker

--- scripts/reset-db.sh
+++ scripts/reset-db.sh
@@ -4,9 +4,9 @@ set -e
 CONTAINER_NAME="postgres_container"
 DB_USER="postgres"
 DB_NAME="postgres"
-BACKUP_BEFORE_RESET="./main/resources/backups/backup_before_reset.sql"
+BACKUP_BEFORE_RESET="../src/main/resources/sql/backup_before_reset.sql"
 # You can view the volume name using the "docker volume ls" command.
-PG_VOLUME="src_pg_data"
+PG_VOLUME="opd_springspree_pg_data"
 
 echo "Creating a backup before reset..."
 docker exec "$CONTAINER_NAME" pg_dump -U "$DB_USER" -d "$DB_NAME" > "$BACKUP_BEFORE_RESET"
