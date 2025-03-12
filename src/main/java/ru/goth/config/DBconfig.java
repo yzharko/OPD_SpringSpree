127774cf06418ad6b755edbf0ec2d7cd4fbf6500
AritoRR <aritorr@mail.ru>
2025-03-13 01:10:00 +0300

    OPD_SS: Убрал Connect и реализовал по другому подключение, добавил в слой репозитория реализацию методов update и readById

--- src/main/java/ru/goth/config/DBconfig.java
+++ src/main/java/ru/goth/config/DBconfig.java
@@ -2,10 +2,16 @@ package ru.goth.config;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.sql.Connection;
+import java.sql.SQLException;
 
 public class DBconfig {
 
     private final static HikariDataSource dataSource;
+    private static final Logger log = LoggerFactory.getLogger(DBconfig.class);
 
     static {
         ConfigLoader configLoader = new ConfigLoader();
@@ -17,6 +23,15 @@ public class DBconfig {
         config.setPassword(configLoader.getPassword());
 
         dataSource = new HikariDataSource(config);
+    }
+
+    public Connection getConnection() {
+        try {
+            return dataSource.getConnection();
+        } catch (SQLException e) {
+            log.error(e.getMessage());
+        }
+        return null;
     }
 
     public static HikariDataSource getDataSource() {
--- src/main/java/ru/goth/domain/entities/City.java
+++ src/main/java/ru/goth/domain/entities/City.java
@@ -57,6 +57,9 @@ public class City {
 
     @Override
     public String toString() {
-        return "City{" + "id=" + id + ", name='" + name + '\'' + ", deliveryTime=" + deliveryTime + '}';
+        return "City{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", deliveryTime=" + deliveryTime + '}';
     }
 }
