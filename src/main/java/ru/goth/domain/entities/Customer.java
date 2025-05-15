9378c7cd46f646f132599a5e7f61f62e049c2a4e
han7ter <sobmisha12@gmail.com>
2025-05-15 18:52:06 +0300

    OPD_SS: кастомер тест написан

--- src/main/java/ru/goth/domain/entities/Customer.java
+++ src/main/java/ru/goth/domain/entities/Customer.java
@@ -6,6 +6,7 @@ import ru.goth.repository.impl.CityRepositoryImpl;
 import ru.goth.service.CityService;
 import ru.goth.service.impl.CityServiceImpl;
 
+import java.sql.Connection;
 import java.util.Objects;
 
 public class Customer {
@@ -25,6 +26,22 @@ public class Customer {
 
     public Customer(String cityName, String name, String email) {
         CityService serv = new CityServiceImpl(new CityRepositoryImpl());
+        Long cityId = serv.existCity(cityName);
+
+        if (cityId == null) {
+            Long time = DeliveryTimeCalculator.getMinutes(cityName);
+            CityDto dto = new CityDto(cityName, time);
+            serv.createCity(dto);
+        }
+
+        cityId = serv.existCity(cityName);
+        this.city_id = cityId;
+        this.name = name;
+        this.email = email;
+    }
+
+    public Customer(Connection connection, String cityName, String name, String email) {
+        CityService serv = new CityServiceImpl(new CityRepositoryImpl(connection));
         Long cityId = serv.existCity(cityName);
 
         if (cityId == null) {
